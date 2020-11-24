c1cb02521292a630251e744f613b879390ae270b
Silas Souza <sillassouza.ads@gmail.com>
2020-11-24 07:15:18 -0300

    continuação na implementação do crud de usuário.

--- aluguelVeiculo/src/main/java/br/fvc/pi3/dao/UsuarioDAO.java
+++ aluguelVeiculo/src/main/java/br/fvc/pi3/dao/UsuarioDAO.java
@@ -2,20 +2,19 @@ package br.fvc.pi3.dao;
 
 import br.fvc.pi3.model.Usuario;
 import br.fvc.pi3.util.JPAutil;
-
 import java.util.List;
-
 import javax.persistence.EntityManager;
+
 /**
  * @author Silas Souza
  */
 public class UsuarioDAO extends JPAutil {
 
 	public Usuario salvar(Usuario usuario) {
-		
+
 		try {
 			EntityManager em = JPAutil.getEntityManager();
-			
+
 			em.getTransaction().begin();
 			em.merge(usuario);
 			em.getTransaction().commit();
@@ -27,30 +26,42 @@ public class UsuarioDAO extends JPAutil {
 			System.out.println("Erro interno ao tentar inserir no banco!");
 		}
 		return usuario;
-
 	}
 
-	
-	public List<Usuario> listaUser() {
-		EntityManager em = JPAutil.getEntityManager();
-		return em.createQuery("select u from Usuario" , Usuario.class).getResultList();
-	}
-	
+
 	public List<Usuario> lista() {
-		
+
 		try {
 			EntityManager em = JPAutil.getEntityManager();
 
 			@SuppressWarnings("unchecked")
 			List<Usuario> listaUsuario = em.createQuery("select u from Usuario u").getResultList();
-			System.out.println(listaUsuario);	
+			System.out.println(listaUsuario);
 			em.close();
 
-			return	listaUsuario;
+			return listaUsuario;
 
 		} catch (Exception e) {
-			System.out.println("Erro interno ao tentar inserção! Log: "+e);
+			System.out.println("Erro interno ao tentar inserção! Log: " + e);
 		}
-		return null;	
+		return null;
+	}
+
+	public Usuario updateMerge(Usuario usuario) {
+		
+		EntityManager em = JPAutil.getEntityManager();
+		em.getTransaction().begin();
+		Usuario entidadeSalva = em.merge(usuario); // O Merge, salva ou atualiza.
+		em.getTransaction().commit();
+
+		return entidadeSalva;
+	}
+	
+	public void delete(Usuario usuario){
+		EntityManager em = JPAutil.getEntityManager();
+		em.getTransaction().begin();
+		em.remove(usuario);
+		em.getTransaction().commit();
+		em.close();
 	}
 }
--- aluguelVeiculo/src/main/java/br/fvc/pi3/ontroller/UsuarioBean.java
+++ aluguelVeiculo/src/main/java/br/fvc/pi3/ontroller/UsuarioBean.java
@@ -2,73 +2,78 @@ package br.fvc.pi3.ontroller;
 
 import br.fvc.pi3.dao.UsuarioDAO;
 import br.fvc.pi3.model.Usuario;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 
 /**
  * @author Silas Souza
  */
 @ManagedBean
-@ViewScoped
-public class UsuarioBean {
+@RequestScoped
+public class UsuarioBean implements Serializable {
 
-    private Usuario usuario;
-    private List<Usuario> usuarios;
-    private UsuarioDAO usuarioDAO;
-    private String destinoSalvar;
+	private static final long serialVersionUID = 1L;
+	private Usuario usuario;
+	private List<Usuario> usuarios;
+	private UsuarioDAO usuarioDAO;
 
-    public UsuarioBean() {
-         usuario = new Usuario();
-         usuarios = new ArrayList<Usuario>();
-         usuarioDAO = new UsuarioDAO();
-    }
+	public UsuarioBean() {
+		usuario = new Usuario();
+		usuarios = new ArrayList<Usuario>();
+		usuarioDAO = new UsuarioDAO();
+	}
 
-    public String novo() {
-        usuario = new Usuario();
-        return "";
-    }
+	public String novo() {
+		this.usuario = new Usuario();
+		this.usuarios.clear();
+		return "";
+	}
 
-    public void excluir() {
-    	usuarios.remove(usuario);
-    }
+	public String remover(){
+		usuarioDAO.delete(usuario);
+		this.getUsuarios();
+		return "";
+	}
 
+	public String editar() {
 
-    public String getEditar() {
-        return "cadastroUsuario";
-    }
-    
-    public void salvarUsuario() {
-    	
-    	if (this.usuario != null) {
-			
-    		this.usuario = usuarioDAO.salvar(this.usuario);
-    		this.usuarios.add(this.usuario);
-    		this.usuario = new Usuario();
-    		
-		}else {
-			
+		this.usuario = usuarioDAO.updateMerge(usuario);
+		
+		return "cadastroUsuario";
+	}
+
+	public String salvarUsuario() {
+
+		if (this.usuario != null) {
+
+			this.usuario = usuarioDAO.salvar(this.usuario);
+
+		} else {
+
 			System.out.println("O objeto usuário está nulo!");
 		}
+		return null;
 
-      /*  if (editar) {
-            this.listaUsuario.set(listaUsuario.indexOf(usuario), usuario);
-        } else {
-           
-            listaUsuario.add(usuario);
-        }*/
-    }
+		/*
+		 * if (editar) { this.listaUsuario.set(listaUsuario.indexOf(usuario), usuario);
+		 * } else {
+		 * 
+		 * listaUsuario.add(usuario); }
+		 */
+	}
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-    
-   
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public List<Usuario> getUsuarios() {
 		usuarios = usuarioDAO.lista();
 		return usuarios;
@@ -78,12 +83,6 @@ public class UsuarioBean {
 		this.usuarios = usuarios;
 	}
 
-	public String getDestinoSalvar() {
-		return destinoSalvar;
-	}
-
-	public void setDestinoSalvar(String destinoSalvar) {
-		this.destinoSalvar = destinoSalvar;
-	}
 	
+
 }
